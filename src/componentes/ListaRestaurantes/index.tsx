b92e4e0f5961a14e3373784c113f3cdeff061afe
Raí <megac154@gmail.com>
2023-09-14 16:13:43 -0300

    pesquisa de restaurante implementado

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -2,17 +2,24 @@ import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { IPaginacao } from '../../interfaces/IPaginacao';
+import { Button, TextField } from '@mui/material';
+
+interface IParametrosBusca {
+  ordering?: string
+  search?: string
+}
 
 const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
   const [paginaAnterior, setPaginaAnterior] = useState('')
+  const [pesquisa, setPesquisa] = useState('')
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url)
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes)
       .then(resposta => {
         // pegando a resposta
         setRestaurantes(resposta.data.results)
@@ -25,12 +32,36 @@ const ListaRestaurantes = () => {
       })
   }
 
+  const pesquisar = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+    const opcoes = {
+      params: {
+
+      } as IParametrosBusca
+    }
+    if (pesquisa) {
+      opcoes.params.search = pesquisa
+    }
+    carregarDados('http://localhost:8000/api/v1/restaurantes/', opcoes)
+  }
+
   useEffect(() => {
     carregarDados("http://localhost:8000/api/v1/restaurantes/")
   }, [])
 
+
   return (
     <section className={style.ListaRestaurantes}>
+      <form onSubmit={pesquisar}>
+        <h2>Pesquise pelo seu restaurante</h2>
+        <TextField 
+          value={pesquisa} 
+          onChange={evento => setPesquisa(evento.target.value)} 
+          label="Pesquisar" 
+          variant="outlined" 
+        />
+        <Button type='submit' variant="contained">Pesquisar</Button>
+      </form>
       <h1>Os restaurantes mais <em>bacanas</em>!</h1>
       {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
       {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}> Página Anterior </button>}
