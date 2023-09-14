262aaf9e43415f14ebbed5b32c6a23de257d23be
Raí <megac154@gmail.com>
2023-09-14 14:43:00 -0300

    refactor ver mais para proxima pagina

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -9,27 +9,15 @@ const ListaRestaurantes = () => {
 
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  useEffect(() => {
-    // obtendo a api
-    axios.get<IPaginacao<IRestaurante>>("http://localhost:8000/api/v1/restaurantes/")
+  const carregarDados = (url: string) => {
+    axios.get<IPaginacao<IRestaurante>>(url)
       .then(resposta => {
         // pegando a resposta
         setRestaurantes(resposta.data.results)
         setProximaPagina(resposta.data.next)
-      })
-      .catch(erro => {
-        // caso dê erro
-        console.log(erro)
-      })
-  }, [])
-
-  const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-      .then(resposta => {
-        // pegando a resposta
-        setRestaurantes([...restaurantes, ...resposta.data.results])
-        setProximaPagina(resposta.data.next)
+        setPaginaAnterior(resposta.data.previous)
       })
       .catch(erro => {
         // caso dê erro
@@ -37,11 +25,16 @@ const ListaRestaurantes = () => {
       })
   }
 
+  useEffect(() => {
+    carregarDados("http://localhost:8000/api/v1/restaurantes/")
+  }, [])
+
   return (
     <section className={style.ListaRestaurantes}>
       <h1>Os restaurantes mais <em>bacanas</em>!</h1>
       {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-      {proximaPagina && <button onClick={verMais}> Ver mais </button>}
+      {<button onClick={() => carregarDados(paginaAnterior)} disabled={!paginaAnterior}> Página Anterior </button>}
+      {<button onClick={() => carregarDados(proximaPagina)} disabled={!proximaPagina}> Próxima página </button>}
     </section>
   )
 }
