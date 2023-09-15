6de4da6586692e78f65de3facb768fbfaa2f4379
Raí <megac154@gmail.com>
2023-09-15 00:16:14 -0300

    refactor axios used basedURL

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import { TableContainer, Paper, Table, TableHead, TableCell, TableRow, TableBody, Button } from "@mui/material"
-import axios from "axios"
 import { Link } from "react-router-dom"
+import http from "../../../http"
 
 const AdministracaoRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
 
   useEffect(() => {
-    axios.get<IRestaurante[]>("http://localhost:8000/api/v2/restaurantes/")
+    http.get<IRestaurante[]>("restaurantes/")
       .then(resposta => {
         setRestaurantes(resposta.data)
       })
   }, [])
 
   const excluir = (restauranteAhSerExcluido: IRestaurante) => {
-    axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteAhSerExcluido.id}/`)
+    http.delete(`restaurantes/${restauranteAhSerExcluido.id}/`)
       .then(() => {
         const listaRestaurante = restaurantes.filter(restaurante => restaurante.id !== restauranteAhSerExcluido.id)
         setRestaurantes([...listaRestaurante])
